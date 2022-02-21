2060814562d8560e68e134c5e08821e0ad25626f
Hunter Klopotek <hunter_klopotek@my.cuesta.edu>
2022-02-20 20:30:49 -0800

    HW 4

---
+++ NumbersInDecending421.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class NumbersInDecending421{
+   public static void main(String[] args){
+      int n;  // n : user input
+      Scanner sc= new Scanner(System.in);
+      n = sc.nextInt(); 
+      for (; n >=0; n--) {
+        System.out.print(n+" ");
+      }
+      sc.close();
+  }
+}
