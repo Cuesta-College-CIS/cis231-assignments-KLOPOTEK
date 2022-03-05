f14a4f824ef485f5c0bc4b1877cad399dd86f3e0
Kyu Lee <kwooler@gmail.com>
2022-03-04 19:41:54 -0800

    lab4

---
+++ Assignment42/lab425.java
@@ -0,0 +1,18 @@
+package Assignment42;
+
+public class lab425 {
+
+	public static void main(String[] args) {
+/*
+		inputString = inputString.toLowerCase();
+		while (i < inputString.length()) {
+			if (Character.isLetter(inputString.charAt(i))) {
+				if (max_char < inputString.charAt(i))
+					max_char = inputString.charAt(i);
+					cnt++;
+				}
+			i++;
+		}
+	}
+*/
+}
