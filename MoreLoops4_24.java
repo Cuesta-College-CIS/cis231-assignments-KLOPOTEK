e09a10121d3900e259ce5367da31f09b4a68ea49
Hunter Klopotek <hunter_klopotek@my.cuesta.edu>
2022-03-05 19:08:31 -0800

    4.24

---
+++ MoreLoops4_24.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MoreLoops4_24 {
+    
+    
+   public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int sum=0;
+    int cnt=0;
+    int x;
+    x = sc.nextInt();
+    if(x<100){
+        while(sum + x <100){
+            sum+=x;
+            cnt++;
+            x =sc.nextInt();
+        }
+    }
+    System.out.printf("%d\t%d\n", sum, cnt);
+}
+}
