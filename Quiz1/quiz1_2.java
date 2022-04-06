21d4f84ea139046ef77ed0c29318276871b42271
Kyu Lee <kwooler@gmail.com>
2022-04-05 18:20:34 -0700

    quiz12-2

--- Quiz1/quiz1_2.java
+++ Quiz1/quiz1_2.java
@@ -6,27 +6,27 @@ public class quiz1_2 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
-        int sum1=0;
-        int sum2=0;
+		int sum1 = 0;
+		int sum2 = 0;
 
-		int[][] matrix1 = { 
-                { 2, 5, 10 },
+		int[][] matrix1 = {
+				{ 2, 5, 10 },
 				{ 3, 6, 9 },
 				{ 1, 2, 3 } };
-		int[][] matrix2 = { 
-                { 10, 20 },
+		int[][] matrix2 = {
+				{ 10, 20 },
 				{ 30, 40 },
 				{ 50, 60 } };
 		int rowidx = scnr.nextInt();
 
-		for(int i =0; i<3;i++){
-           sum1 += matrix1[rowidx][i] * matrix2[i][0];
-        }
-        for(int i =0; i<3;i++){
-            sum2 += matrix1[rowidx][i] * matrix2[i][1];
-        }
-        System.out.println(sum1+" "+sum2);
-        scnr.close();
+		for (int i = 0; i < 3; i++) {
+			sum1 += matrix1[rowidx][i] * matrix2[i][0];
+		}
+		for (int i = 0; i < 3; i++) {
+			sum2 += matrix1[rowidx][i] * matrix2[i][1];
+		}
+		System.out.println(sum1 + " " + sum2);
+		scnr.close();
 	}
 
 }
